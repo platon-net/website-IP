568c5ba5efd2fd61ab846c4cf164b1753054c1b1
igor <mino@tpsoft.org>
2024-07-16 10:20:59 +0200

    added build addon with windows batch

---
+++ build-addon.bat
@@ -0,0 +1,17 @@
+@echo off
+rem cd .\src
+
+rem copy manifest.chrome.json manifest.json
+for /f "delims=" %%a in ('findstr "\"version\"" manifest.json') do @set versionrow=%%a
+set version=%versionrow:~13,5%
+tar -a -c -f build\chrome\WebsiteIP.%version%.zip --exclude "*.bat" --exclude "*.psd" --exclude "*.eps" --exclude "manifest.chrome.json" --exclude "manifest.firefox.json" --exclude ".git" --exclude "build" *
+
+rem copy manifest.firefox.json manifest.json
+for /f "delims=" %%a in ('findstr "\"version\"" manifest.json') do @set versionrow=%%a
+set version=%versionrow:~13,5%
+tar -a -c -f build\firefox\WebsiteIP.%version%.zip --exclude "*.bat" --exclude "*.psd" --exclude "*.eps" --exclude "manifest.chrome.json" --exclude "manifest.firefox.json" --exclude ".git" --exclude "build" *
+copy build\firefox\WebsiteIP.%version%.zip build\firefox\WebsiteIP.%version%.xpi
+del build\firefox\WebsiteIP.%version%.zip
+
+rem cd ..
+pause
