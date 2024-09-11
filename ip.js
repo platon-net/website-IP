3ecb567f65318c9aaee8f9e983aa78765bc0500e
igor <mino@tpsoft.org>
2024-09-11 17:21:12 +0200

    disabled right <-> left switch popup, added drag&drop to move popup, upgrade to version 1.2.2

--- ip.js
+++ ip.js
@@ -13,7 +13,7 @@ browser.runtime.sendMessage({name: 'isEnabled'}, function(isEnabled) {
 		var websiteip = document.createElement('websiteip');
 
 		websiteip.id = 'chrome_websiteIP';
-		websiteip.className = 'chrome_websiteIP_right';
+		//websiteip.className = 'chrome_websiteIP_right';
 		var response_ip = response.ip;
 		if (response_ip == null
 			|| response_ip == undefined)
@@ -25,6 +25,7 @@ browser.runtime.sendMessage({name: 'isEnabled'}, function(isEnabled) {
 			document.body.appendChild(websiteip);
 		}
 
+		/*
 		websiteip.addEventListener('mouseover', function() {
 			if(this.className.indexOf('chrome_websiteIP_right') !== -1) {
 				this.className = this.className.replace('chrome_websiteIP_right', 'chrome_websiteIP_left');
@@ -32,5 +33,41 @@ browser.runtime.sendMessage({name: 'isEnabled'}, function(isEnabled) {
 				this.className = this.className.replace('chrome_websiteIP_left', 'chrome_websiteIP_right');
 			}
 		}, false);
+		*/
+		dragElement(websiteip);
 	});
 });
+
+function dragElement(elmnt) {
+	var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+	elmnt.onmousedown = dragMouseDown;
+
+	function dragMouseDown(e) {
+		e = e || window.event;
+		e.preventDefault();
+		// Získaj pozíciu kurzora na začiatku
+		pos3 = e.clientX;
+		pos4 = e.clientY;
+		document.onmouseup = closeDragElement;
+		document.onmousemove = elementDrag;
+	}
+
+	function elementDrag(e) {
+		e = e || window.event;
+		e.preventDefault();
+		// Vypočítaj novú pozíciu kurzora
+		pos1 = pos3 - e.clientX;
+		pos2 = pos4 - e.clientY;
+		pos3 = e.clientX;
+		pos4 = e.clientY;
+		// Nastav novú pozíciu elementu
+		elmnt.style.top = (Math.max(elmnt.offsetTop - pos2, 0)) + "px";
+		elmnt.style.left = (Math.max(elmnt.offsetLeft - pos1, 0)) + "px";
+	}
+
+	function closeDragElement() {
+		// Zastav posúvanie, keď sa uvoľní tlačidlo myši
+		document.onmouseup = null;
+		document.onmousemove = null;
+	}
+}
